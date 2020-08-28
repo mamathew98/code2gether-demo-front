37d915f402127eb7f950069d7cd2778128c42c1c
MO Naseri <mohammad.na3ri@gmail.com>
2020-08-28 18:31:23 +0430

    .

--- dist/main-es5.js
+++ dist/main-es5.js
@@ -6986,7 +6986,7 @@
 
       var environment = {
         production: false,
-        apiUrl: 'http://localhost/api'
+        apiUrl: 'http://51.195.28.68/api'
       };
       /*
        * For easier debugging in development mode, you can import the following file
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost/api'
+    apiUrl: 'http://51.195.28.68/api'
 };
